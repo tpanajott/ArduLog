debb4cb7b0e72dc2dc3f27009c3ac52f0dae58a8
Tim Panajott <vpklotar@gmail.com>
2022-10-08 10:33:14 +0200

    Added another example for logging with colors

---
+++ examples/advanced_with_colors.cpp
@@ -0,0 +1,22 @@
+// Uncomment the following to enable decorations in log output
+// Use a compatible serial monitor, in platformio set
+// monitor_raw = 1
+#define ARDULOG_USE_DECORATIONS 1
+
+#include <ArduLog.h>
+#include <Arduino.h>
+
+ArduLog logger;
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  logger.init();
+  logger.SetSerial(&Serial);
+  logger.SetLogLevel(ArduLogLevel::Trace);
+}
+
+void loop() {
+  LOG_DEBUG(LOG_BLUE, "Seconds since boot: ", LOG_GREEN, millis() / 1000);
+  delay(1000);
+}
